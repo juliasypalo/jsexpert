38de54fb5c04bc3b215e10ad1465f56a9ecbb188
juliasypalo <35544495+juliasypalo@users.noreply.github.com>
2018-01-17 23:55:23 +0200

    Create julia-test.js

---
+++ julia-test.js
@@ -0,0 +1,18 @@
+(function (win) {
+    var params = {
+        states: {
+            url: "/",
+            template: "temlate.js"
+        }
+    };
+
+    function setStates(params) {
+        if (win && !win.params) {
+            win.params = params;
+        }
+    }
+
+    setStates();
+
+    console.log(params.states.template);
+})(window);
